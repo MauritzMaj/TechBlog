bb5e7c1242ccd4ab11a61a215de2676061a11ca7
MauritzMaj <mauritz.majelyne@hotmail.com>
2023-02-16 15:41:00 -0500

    first commit

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#display_name').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (name && password) {
+      const response = await fetch('/api/users/session', {
+        method: 'POST',
+        body: JSON.stringify({display_name:name, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Incorrect email or password!');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
